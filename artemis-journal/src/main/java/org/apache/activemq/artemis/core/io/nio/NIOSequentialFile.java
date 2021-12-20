5fa668e24f2aefa1451b411eef9af56930b02e92
Rico Neubauer <r.neubauer@seeburger.de>
2021-12-20 14:17:54 +0100

    ARTEMIS-2618 [Feature #91734] Improve Handling of Shutdown

--- artemis-journal/src/main/java/org/apache/activemq/artemis/core/io/nio/NIOSequentialFile.java
+++ artemis-journal/src/main/java/org/apache/activemq/artemis/core/io/nio/NIOSequentialFile.java
@@ -63,6 +63,10 @@ public class NIOSequentialFile extends AbstractSequentialFile {
     */
    private static final int CHUNK_SIZE = 2 * 1024 * 1024;
 
+   private static final int RETRIES_MAX = Integer.getInteger("org.apache.activemq.artemis.core.io.nio.NIOSequentialFile.RETRIES_MAX", 5).intValue();
+
+   private static final long RETRIES_TIMEOUT = Long.getLong("org.apache.activemq.artemis.core.io.nio.NIOSequentialFile.RETRIES_TIMEOUT", 15000).intValue();
+
    protected volatile  FileChannel channel;
 
    protected volatile RandomAccessFile rfile;
@@ -143,7 +147,37 @@ public class NIOSequentialFile extends AbstractSequentialFile {
    @Override
    public void open(final int maxIO, final boolean useExecutor) throws IOException {
       try {
-         rfile = new RandomAccessFile(getFile(), "rw");
+         int tries = 0;
+         long timePassed;
+         long start = System.currentTimeMillis();
+         IOException fatalEx;
+         do {
+            tries++;
+            try {
+               rfile = new RandomAccessFile(getFile(), "rw");
+               fatalEx = null;
+               break; // all good
+            }
+            catch (IOException ex) {
+               fatalEx = ex;
+               logger.error("Error opening file=" + getFileName() +
+                     "! This is fatal and server shut-down is imminent! Will retry up to " + RETRIES_MAX +
+                     " times before fatal error, current retry=" + (tries-1) + "! Ex=" + ex);
+               try {
+                  long sleepTime = Math.min(5000, (2 * (long)Math.pow(10d, tries))); // 20, 200, 2000, 5000ms
+                  Thread.sleep(sleepTime);
+               }
+               catch (InterruptedException intEx) {
+                  Thread.currentThread().interrupt();
+               }
+               timePassed = System.currentTimeMillis() - start;
+            }
+         }
+         while (tries <= RETRIES_MAX && timePassed < RETRIES_TIMEOUT);
+         if (fatalEx != null) {
+            logger.error("Error opening file=" + getFileName() + "! This is fatal and server shut-down is imminent!", fatalEx);
+            throw fatalEx;
+         }
 
          channel = rfile.getChannel();
 
